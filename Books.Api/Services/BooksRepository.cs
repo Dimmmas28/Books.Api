25f66bd3178be8f56212b36177a9d4e50f25a372
Dmytro <savuliak4556664444@gmail.com>
2020-04-15 21:06:59 +0300

    Add project files.

---
+++ Books.Api/Services/BooksRepository.cs
@@ -0,0 +1,34 @@
+﻿using Books.Api.Context;
+using Books.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Services
+{
+    public class BooksRepository : IBooksRepository, IDisposable
+    {
+        private BooksContext _context;
+
+        public BooksRepository(BooksContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<Book> GetBookAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            throw new NotImplementedException();
+        } 
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Books.Api/Services/IBooksRepository.cs
@@ -0,0 +1,16 @@
+﻿using Books.Api.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Books.Api.Services
+{
+    interface IBooksRepository
+    {
+        //IEnumerable<Book> GetBooks();
+        //Book GetBook();
+
+        Task<IEnumerable<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync();
+    }
+}
